4e739a90d5430a8678dfc08fe3fc7641c508832d
wq <576503389@qq.com>
2017-04-07 13:37:42 +0800

    add some

--- src/js/flex.js
+++ src/js/flex.js
@@ -2,7 +2,15 @@
  * Created by wq on 2017/4/6.
  */
 (function (win, doc, math) {
-    var k = win.innerWidth, dpr = win.devicePixelRatio, designDraftWidth = 750, designDraftFontSize = 28, fontSize = '12px', docEl = doc.documentElement;
+    var k = win.innerWidth,
+        dpr = win.devicePixelRatio,
+        designDraftWidth = 750,
+        designDraftFontSize = 28,
+        fontSize = '12px',
+        docEl = doc.documentElement,
+        isIPhone = win.navigator.appVersion.match(/iphone/gi),
+        borderWidth = '1px';
+
     docEl.style.fontSize = designDraftFontSize * k / designDraftWidth + 'px';
     docEl.style.maxWidth = designDraftWidth + 'px';
     if (dpr >= 2 && dpr < 3) {
@@ -12,5 +20,8 @@
     } else {
         fontSize = '16px';
     }
-    doc.write('<style>body{font-size:' + fontSize + ';}</style>');
+    if(isIPhone){
+        borderWidth = (1 / designDraftFontSize < 1 / dpr ? 1 / dpr : 1 / designDraftFontSize) + 'rem';
+    }
+    doc.write('<style>body{font-size:' + fontSize + ';}.bw{border-width:'+borderWidth+';}</style>');
 }(window, document, Math))
