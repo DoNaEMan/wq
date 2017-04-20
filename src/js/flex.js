eacdab6ca928537253f12a856af4205ea6bd580b
wq <576503389@qq.com>
2017-04-20 17:01:52 +0800

    add some

--- src/js/flex.js
+++ src/js/flex.js
@@ -5,13 +5,13 @@
     var k = win.innerWidth,
         dpr = win.devicePixelRatio,
         designDraftWidth = 750,
-        designDraftFontSize = 28,
+        d = 100;
         fontSize = '12px',
         docEl = doc.documentElement,
         isIPhone = win.navigator.appVersion.match(/iphone/gi),
         borderWidth = '1px';
 
-    docEl.style.fontSize = designDraftFontSize * k / designDraftWidth + 'px';
+    docEl.style.fontSize = d * k / designDraftWidth + 'px';
     docEl.style.maxWidth = designDraftWidth + 'px';
     if (dpr >= 2 && dpr < 3) {
         fontSize = '14px';
@@ -21,7 +21,7 @@
         fontSize = '16px';
     }
     if(isIPhone){
-        borderWidth = (1 / designDraftFontSize) + 'rem';
+        borderWidth = (1 / d) + 'rem';
     }
-    doc.write('<style>body{font-size:' + fontSize + ';}.bw{border-width:'+borderWidth+';}</style>');
+    doc.write('<style>body{font-size:' + fontSize + ';margin: 0 auto;}.bw{border-width:'+borderWidth+';}</style>');
 }(window, document, Math))
