c2a2ce20a5f7ae11e5fd461055d03c4bc03e26f4
wq <576503389@qq.com>
2017-06-12 20:15:35 +0800

    add some

--- src/js/flex.js
+++ src/js/flex.js
@@ -2,17 +2,33 @@
  * Created by wq on 2017/4/6.
  */
 (function (win, doc, math) {
-    var k = win.innerWidth,
-        dpr = win.devicePixelRatio,
-        designDraftWidth = 750,
-        d = 100;
-        fontSize = '12px',
-        docEl = doc.documentElement,
-        isIPhone = win.navigator.appVersion.match(/iphone/gi),
-        borderWidth = '1px';
+    var sw ,timer;
+    var designDefautRatio = 100
+	var designDraftWidth = 750;
+	var dpr = win.devicePixelRatio;
+    var designDefautRatio = 100;
+	var fontSize = '12px';
+    var docEl = doc.documentElement;
+    var isIPhone = win.navigator.appVersion.match(/iphone/gi);
+	var borderWidth = '1px';
 
-    docEl.style.fontSize = d * k / designDraftWidth + 'px';
-    docEl.style.maxWidth = designDraftWidth + 'px';
+	docEl.style.maxWidth = designDraftWidth + 'px';
+	function getInfo(){
+		sw = win.innerWidth;
+		docEl.style.fontSize = designDefautRatio * sw / designDraftWidth + 'px';
+    }
+
+    getInfo();
+
+	function flex() {
+		clearTimeout(timer);
+		setTimeout(function () {
+			getInfo();
+		}, 300);
+	}
+	window.addEventListener('orientationchange',flex,false);
+	window.addEventListener('resize', flex,false);
+
     if (dpr >= 2 && dpr < 3) {
         fontSize = '14px';
     } else if (dpr >= 1 && dpr < 2) {
@@ -21,7 +37,7 @@
         fontSize = '16px';
     }
     if(isIPhone){
-        borderWidth = (1 / d) + 'rem';
+        borderWidth = (1 / designDefautRatio) + 'rem';
     }
     doc.write('<style>body{font-size:' + fontSize + ';margin: 0 auto;}.bw{border-width:'+borderWidth+';}</style>');
 }(window, document, Math))
